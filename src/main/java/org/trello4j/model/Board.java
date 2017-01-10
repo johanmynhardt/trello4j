73a5cb23ee516add49e39fd842c449ae3833ec30
Guilherme Lima Pereira <guuuilp@gmail.com>
2017-01-10 00:26:47 -0200

    Organizing class content

--- src/main/java/org/trello4j/model/Board.java
+++ src/main/java/org/trello4j/model/Board.java
@@ -2,14 +2,14 @@
 package org.trello4j.model;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.trello4j.gson.CopyOfPermissionTypeDeserializer;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-public class Board {
+public class Board extends TrelloObject {
+
     @JsonDeserialize(using = CopyOfPermissionTypeDeserializer.class)
     public enum PERMISSION_TYPE {
         PUBLIC, ORGANIZATION, MEMBERS
@@ -20,22 +20,9 @@ public class Board {
     private Object descData;
     private Boolean closed;
     private String idOrganization;
-    private Object pinned;
-    private Object invitations;
-    private String shortLink;
-    private List<String> powerUps = null;
-    private String dateLastActivity;
-    private List<Object> idTags = null;
-    private String id;
-    private Boolean invited;
-    private Boolean starred;
+    private Boolean pinned;
     private String url;
     private Prefs prefs;
-    private List<Membership> memberships = null;
-    private Boolean subscribed;
-    private LabelNames labelNames;
-    private String dateLastView;
-    private String shortUrl;
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     public String getName() {
@@ -78,76 +65,12 @@ public class Board {
         this.idOrganization = idOrganization;
     }
 
-    public Object getPinned() {
+    public Boolean getPinned() {
         return pinned;
     }
 
-    public void setPinned(Object pinned) {
+    public void setPinned(Boolean pinned) {
         this.pinned = pinned;
-    }
-
-    public Object getInvitations() {
-        return invitations;
-    }
-
-    public void setInvitations(Object invitations) {
-        this.invitations = invitations;
-    }
-
-    public String getShortLink() {
-        return shortLink;
-    }
-
-    public void setShortLink(String shortLink) {
-        this.shortLink = shortLink;
-    }
-
-    public List<String> getPowerUps() {
-        return powerUps;
-    }
-
-    public void setPowerUps(List<String> powerUps) {
-        this.powerUps = powerUps;
-    }
-
-    public String getDateLastActivity() {
-        return dateLastActivity;
-    }
-
-    public void setDateLastActivity(String dateLastActivity) {
-        this.dateLastActivity = dateLastActivity;
-    }
-
-    public List<Object> getIdTags() {
-        return idTags;
-    }
-
-    public void setIdTags(List<Object> idTags) {
-        this.idTags = idTags;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Boolean getInvited() {
-        return invited;
-    }
-
-    public void setInvited(Boolean invited) {
-        this.invited = invited;
-    }
-
-    public Boolean getStarred() {
-        return starred;
-    }
-
-    public void setStarred(Boolean starred) {
-        this.starred = starred;
     }
 
     public String getUrl() {
@@ -164,46 +87,6 @@ public class Board {
 
     public void setPrefs(Prefs prefs) {
         this.prefs = prefs;
-    }
-
-    public List<Membership> getMemberships() {
-        return memberships;
-    }
-
-    public void setMemberships(List<Membership> memberships) {
-        this.memberships = memberships;
-    }
-
-    public Boolean getSubscribed() {
-        return subscribed;
-    }
-
-    public void setSubscribed(Boolean subscribed) {
-        this.subscribed = subscribed;
-    }
-
-    public LabelNames getLabelNames() {
-        return labelNames;
-    }
-
-    public void setLabelNames(LabelNames labelNames) {
-        this.labelNames = labelNames;
-    }
-
-    public String getDateLastView() {
-        return dateLastView;
-    }
-
-    public void setDateLastView(String dateLastView) {
-        this.dateLastView = dateLastView;
-    }
-
-    public String getShortUrl() {
-        return shortUrl;
-    }
-
-    public void setShortUrl(String shortUrl) {
-        this.shortUrl = shortUrl;
     }
 
     public Map<String, Object> getAdditionalProperties() {
