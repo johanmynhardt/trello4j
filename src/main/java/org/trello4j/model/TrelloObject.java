9c0af51a2f66e74e98459b002211784c16d76f8c
Guilherme Lima Pereira <guuuilp@gmail.com>
2017-01-10 00:25:03 -0200

    Organizing class content

--- src/main/java/org/trello4j/model/TrelloObject.java
+++ src/main/java/org/trello4j/model/TrelloObject.java
@@ -8,21 +8,18 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class TrelloObject {
 
-	private String id;
+    private String id;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	@Override
-	public String toString() {
-		return new StringBuilder(getClass().getSimpleName())
-				.append(" ")
-				.append(id)
-				.toString();
-	}
+    @Override
+    public String toString() {
+        return new StringBuilder(getClass().getSimpleName()).append(" ").append(id).toString();
+    }
 }
