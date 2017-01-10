037fe908ddfed5f3d68889d49e1af6863cd405a8
Guilherme Lima Pereira <guuuilp@gmail.com>
2017-01-10 00:24:25 -0200

    Organizing class content

--- src/main/java/org/trello4j/model/Organization.java
+++ src/main/java/org/trello4j/model/Organization.java
@@ -1,66 +1,56 @@
 package org.trello4j.model;
 
 /**
- * <code>
- * {
- * 	"id":"4eb3f3f1e679eb839b4c594b",
- * 	"name":"frostdigital",
- * 	"displayName":"Frost Digital",
- * 	"desc":"",
- * 	"url":"https://trello.com/frostdigital",
- * 	"website":"http://frostdigital.se"
- * }
- * </code>
  * 
  * @author joel
- * 
+ *
  */
 public class Organization extends TrelloObject {
 
-	private String name;
-	private String displayName;
-	private String desc;
-	private String website;
-	private String url;
+    private String name;
+    private String displayName;
+    private String desc;
+    private String website;
+    private String url;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDisplayName() {
-		return displayName;
-	}
+    public String getDisplayName() {
+        return displayName;
+    }
 
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 
-	public String getDesc() {
-		return desc;
-	}
+    public String getDesc() {
+        return desc;
+    }
 
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 
-	public String getWebsite() {
-		return website;
-	}
+    public String getWebsite() {
+        return website;
+    }
 
-	public void setWebsite(String website) {
-		this.website = website;
-	}
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
 }
